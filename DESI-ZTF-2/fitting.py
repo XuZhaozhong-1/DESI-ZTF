319c236daf7d50674f57f92ff9cc95fb76b9ecad
Zhaozhong Xu <zhaozhon@x1403c0s7b0n1h0.chn.perlmutter.nersc.gov>
2025-03-05 06:23:26 -0800

    update

--- DESI-ZTF-2/fitting.py
+++ DESI-ZTF-2/fitting.py
@@ -290,7 +290,8 @@ class ln_posterior(object):
         self.beta = -(self.gamma1+1) + 0.5
         self.alpha = -(self.gamma2+1)
 
-    def __call__(self, m0, b, x, mhat, k, mu,sigma,fraction):
+    def __call__(self, m0, c, x, mhat, k, mu,sigma,fraction):
+        b = c*m0
         nquasar=len(mhat)
         M = self._y[:,None] * sigma + mhat - x - k - mu
         #df = df_analytic(m0,b,M+x+k+mu,sigma)
